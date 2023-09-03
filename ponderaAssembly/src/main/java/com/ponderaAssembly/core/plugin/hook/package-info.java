7e84e5301d468fabf33253e04d9e1a4ff47d0209
ivan <ivangavlik963@gmail.com>
2023-09-03 16:52:16 +0200

    github pages init

--- ponderaAssembly/src/main/java/com/ponderaAssembly/core/plugin/hook/package-info.java
+++
@@ -1,1 +0,0 @@
-package com.ponderaAssembly.core.plugin.hook;
--- ponderaAssembly/src/test/java/com/ponderaAssembly/core/data/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Contains class thar are used for testing as dependency and plugin instance.
- */
-package com.ponderaAssembly.core.data;
